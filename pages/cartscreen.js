78768944b563d74cfff762a255e8842047d2869b
Afif-Mac <afifmansib123@gmail.com>
2023-05-25 16:47:32 +0700

    hello

--- pages/cartscreen.js
+++ pages/cartscreen.js
@@ -11,4 +11,44 @@ export default function Cartscreen () {
 
     console.log('putkimari')
     console.log(JSON.stringify(cartitems))
+
+    return(
+        <Layout>
+            <div className="grid md:grid-cols-5 md:gap-5">
+            <div className="overflow-x-auto md: col-span-3">
+            <table className="min-w-full">
+                <thead className="border-b">
+                    <tr>
+                    <th>Item</th>
+                    <th>Item</th>
+                    <th>Item</th>
+                    <th>Item</th>
+                    </tr>
+                </thead>
+                <tbody>
+  {cartitems.map((item) => (
+    <tr key={item.slug} className="border-b">
+      <td>
+        <Link legacyBehavior href={`/products/${item.slug}`}>
+          <a className="flex items-center">
+            <Image
+              src={item.image}
+              alt={item.name}
+              width={600}
+              height={600}
+            />
+            &nbsp;{item.name}
+          </a>
+        </Link>
+      </td>
+      <td className="p-5 text-right">{item.quantity}</td>
+      <td className="p-5 text-right">{item.price}</td>
+    </tr>
+  ))}
+</tbody>
+            </table>
+            </div>
+            </div>
+        </Layout>
+    )
 }
